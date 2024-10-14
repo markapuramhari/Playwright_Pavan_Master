e986622fee8d0bd78d2772b6f90b99fb15937ee8
hari <markapuramhari@gmail.com>
2024-10-14 18:33:19 +0530

    update in Part 04 Multipule Elements

--- tests/Part_04_LocatingMultipuleElements.spec.js
+++ tests/Part_04_LocatingMultipuleElements.spec.js
@@ -5,14 +5,14 @@ test("Mutipule Elements",async ({page})=>{
 
     await page.goto('https://www.demoblaze.com/index.html')
 
-    /*
+    
     const links= await page.$$('a')
     for(const link of links){
 
         const linktext= await link.textContent()
-        console.log(linktext)
+        console.log('All links: '+linktext)
     }
-    */
+    
 
     await page.waitForSelector("//div[@id='tbodyid']//div//h4/a")
     const products= await page.$$("//div[@id='tbodyid']//div//h4/a")
