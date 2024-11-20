1942120f5e85ba8e4ac1656dbdf2ffbb7169c0bd
hari <markapuramhari@gmail.com>
2024-11-20 13:49:40 +0530

    updated WindowHandling

--- tests/Part_35_HandlingWindows.spec.js
+++ tests/Part_35_HandlingWindows.spec.js
@@ -21,6 +21,7 @@ test('Part 35 WindowHandling 2 Tabs Test', async ({ playwright }) => {
 
 test('Part 35 Window Handling 1 Tab Test', async ({ playwright }) => {
 
+    test.slow()
     const browser = await playwright.chromium.launch({ channel: 'chrome' })
     const context = await browser.newContext()
     const page1 = await context.newPage()
@@ -30,17 +31,22 @@ test('Part 35 Window Handling 1 Tab Test', async ({ playwright }) => {
 
     await page1.click("//a[normalize-space()='OrangeHRM, Inc']")
     const pagePromise = context.waitForEvent('page')
+    
     const page2 = await pagePromise
     await expect(page2).toHaveTitle('Human Resources Management Software | OrangeHRM')
     const text = page2.locator("//div[@class='homepage-slider-content']/h1")
     await expect(text).toContainText('clicks away!')
+    
+    await page2.waitForSelector("//img[@alt='linkedin logo']")
 
-
+    if(await page2.click('#CybotCookiebotDialogBodyButtonAccept')){
+        console.log('Cookies cliked')
+    }
+    
     await page2.click("//img[@alt='linkedin logo']")
     const pagePromisee = context.waitForEvent('page')
     const page3 = await pagePromisee
     const LinkedIntext = page3.locator('.authwall-join-form__title')
-    await page3.waitForSelector('.authwall-join-form__title')
     await expect(LinkedIntext).toContainText('LinkedIn')
     console.log('LinkedIntext:', await LinkedIntext.textContent())
     await page3.close()
