58a004b1c111594df29829dd878b132d3ed691ba
hari <markapuramhari@gmail.com>
2024-11-23 22:04:13 +0530

    cross-env Login added

--- tests/Part_04_LoginpageTest.spec.js
+++ tests/Part_04_LoginpageTest.spec.js
@@ -2,12 +2,15 @@
 import { test, expect } from '@playwright/test'
 test('Part 04 Loginpage Test', { tag: ['@03To05', '@03To10'] }, async ({ page }) => {
 
-	await page.goto('https://www.demoblaze.com/index.html')
+	console.log('URL:',process.env.URL)
+	await page.goto(process.env.URL)
 
 	//await page.getByRole('link', { name: 'Log in' }).click()
 	await page.locator("//a[@id='cartur']/parent::li/following-sibling::li/a[@id='login2']").click()
-	await page.fill('#loginusername', 'markapuram')
-	await page.fill('#loginpassword', 'markapuram')
+	console.log('UN:',process.env.UN)
+	await page.fill('#loginusername', process.env.UN)
+	console.log('PWD:',process.env.PWD)
+	await page.fill('#loginpassword', process.env.PWD)
 	await page.getByRole('button', { name: 'Log in' }).click()
 	await page.getByRole('link', { name: 'Log out' }).click()
 
